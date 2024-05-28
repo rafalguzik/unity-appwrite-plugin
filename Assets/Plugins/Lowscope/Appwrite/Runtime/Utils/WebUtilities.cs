58bac47e93d16a29179d900f733f97db149a7255
Rafal Guzik <rafal.guzik@explodedview.io>
2024-05-28 11:57:18 +0200

    removed System.Web dependency

--- Assets/Plugins/Lowscope/Appwrite/Runtime/Utils/WebUtilities.cs
+++ Assets/Plugins/Lowscope/Appwrite/Runtime/Utils/WebUtilities.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 
 namespace Lowscope.AppwritePlugin.Utils
@@ -10,6 +14,16 @@ namespace Lowscope.AppwritePlugin.Utils
 				new Regex(
 					@"^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$");
 			return regex.Match(email).Success;
+		}
+
+		public static string ToQueryString(NameValueCollection nvc)
+		{
+			IEnumerable<string> segments = from key in nvc.AllKeys
+										from value in nvc.GetValues(key)
+										select string.Format("{0}={1}", 
+										WebUtility.UrlEncode(key),
+										WebUtility.UrlEncode(value));
+			return "?" + string.Join("&", segments);
 		}
 	}
 }
